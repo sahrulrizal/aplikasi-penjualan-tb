079ec67557ab64cbc6ef87c9fd38b3f57d653fab
Sahrul rizal <sahrulrizal22@gmail.com>
2014-05-14 13:32:06 +0700

    barang,penjualan,penjualandetail

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/Penjualan.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/domain/Penjualan.java
@@ -1,9 +1,9 @@
 package net.smktarunabhakti.penjualan.domain;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,64 +11,79 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
-
+import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.mapping.Value;
 
+/**
+ *
+ * @author jimmy
+ */
 @Entity
-@Table(name="trx_jual_header")
-public class Penjualan implements Serializable{
-@Id
-@GeneratedValue(generator = "system.uuid")
-@GenericGenerator(name = "system.uuid", strategy = "uuid")
-	private String id;
-	
-	@Column(name="waktu_transaksi")
-	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
-	private Date tgl;
-	
-	@Column(nullable = false)
-	private String kasir;
-	
-	@Column(name="no_struk", nullable = false)
-	private String noStruk;
+@Table(name = "trx_jual_header")
+public class Penjualan implements Serializable {
 
-	@OneToMany(mappedBy = "header")
-	@Cascade( value = CascadeType.ALL )
-	private List<PenjualanDetail> listPenjualanDetail;
-	
-	public String getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    @NotNull
+    @Column(name = "waktu_transaksi", nullable = false)
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date tgl;
 
-	public Date getTgl() {
-		return tgl;
-	}
+    @NotNull
+    @Column(nullable = false)
+    private String kasir;
 
-	public void setTgl(Date tgl) {
-		this.tgl = tgl;
-	}
+    @NotNull
+    @Column(name = "no_struk", nullable = false)
+    private String noStruk;
 
-	public String getKasir() {
-		return kasir;
-	}
+    @OneToMany(mappedBy = "header")
+    @Cascade(value = CascadeType.ALL)
+    private List<PenjualanDetail> listPenjualanDetail = new ArrayList<PenjualanDetail>();
 
-	public void setKasir(String kasir) {
-		this.kasir = kasir;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getNoStruk() {
-		return noStruk;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setNoStruk(String noStruk) {
-		this.noStruk = noStruk;
-	}
-	
-	
+    public Date getTgl() {
+        return tgl;
+    }
+
+    public void setTgl(Date tgl) {
+        this.tgl = tgl;
+    }
+
+    public String getKasir() {
+        return kasir;
+    }
+
+    public void setKasir(String kasir) {
+        this.kasir = kasir;
+    }
+
+    public String getNoStruk() {
+        return noStruk;
+    }
+
+    public void setNoStruk(String noStruk) {
+        this.noStruk = noStruk;
+    }
+
+    public List<PenjualanDetail> getListPenjualanDetail() {
+        return listPenjualanDetail;
+    }
+
+    public void setListPenjualanDetail(List<PenjualanDetail> listPenjualanDetail) {
+        this.listPenjualanDetail = listPenjualanDetail;
+    }
+
 }
